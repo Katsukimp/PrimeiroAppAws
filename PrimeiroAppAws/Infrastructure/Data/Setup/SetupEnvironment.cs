735014d5310e38780b7ca407a0583997a45b0a01
eduardokm <eduardokm@opcaovirtual.com.br>
2023-09-02 19:38:28 -0400

    "Feat - Created Setup Layer for Infrastructure"

---
+++ PrimeiroAppAws/Infrastructure/Data/Setup/SetupEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiroAppAws.Domain.Interfaces;
+using PrimeiroAppAws.Infrastructure.Data.Commom;
+using PrimeiroAppAws.Infrastructure.Data.Commom.Interfaces;
+using PrimeiroAppAws.Infrastructure.Data.Repositories;
+
+namespace PrimeiroAppAws.Infrastructure.Data.Setup
+{
+    public static class SetupEnvironment
+    {
+        public static IServiceCollection AddUnitOfWork(this IServiceCollection services) => services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services) => 
+            services
+                .AddTransient<IBlogRepository, BlogRepository>()
+                .AddTransient<IPostRepository, PostRepository>();
+
+        public static IServiceCollection AddContext(this IServiceCollection services) => services.AddDbContext<RegisterContext>(x => x.UseInMemoryDatabase("Teste"));
+    }
+}
