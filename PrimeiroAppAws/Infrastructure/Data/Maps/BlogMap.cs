6301bc02e97789cc07231ac6a671fc9f3cd2f6d6
eduardokm <eduardokm@opcaovirtual.com.br>
2023-09-02 19:36:16 -0400

    Feat - Createad Maps

---
+++ PrimeiroAppAws/Infrastructure/Data/Maps/BlogMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrimeiroAppAws.Domain.Entities;
+
+namespace PrimeiroAppAws.Infrastructure.Data.Maps
+{
+    public class BlogMap : IEntityTypeConfiguration<Blog>
+    {
+        public void Configure(EntityTypeBuilder<Blog> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.CreatedAt);
+
+            builder.Property(x => x.Active);
+
+            builder.Property(x => x.Description);
+
+            builder.HasMany(x => x.Posts).WithOne();
+        }
+    }
+}
---
+++ PrimeiroAppAws/Infrastructure/Data/Maps/PostMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrimeiroAppAws.Domain.Entities;
+
+namespace PrimeiroAppAws.Infrastructure.Data.Maps
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.CreatedAt);
+
+            builder.Property(x => x.Active);
+
+            builder.Property(x => x.Description);
+        }
+    }
+}
