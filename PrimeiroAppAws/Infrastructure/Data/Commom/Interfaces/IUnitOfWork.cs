27e5aa6e5654d278bbdbb07a9ffb07b27b97814c
eduardokm <eduardokm@opcaovirtual.com.br>
2023-09-02 19:32:38 -0400

    Feat - Created Unit of work

---
+++ PrimeiroAppAws/Infrastructure/Data/Commom/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace PrimeiroAppAws.Infrastructure.Data.Commom.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync(CancellationToken cancellationToken);
+        Task RollbackAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ PrimeiroAppAws/Infrastructure/Data/Commom/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using PrimeiroAppAws.Infrastructure.Data.Commom.Interfaces;
+
+namespace PrimeiroAppAws.Infrastructure.Data.Commom
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RegisterContext _context;
+
+        public UnitOfWork(RegisterContext context) => _context = context;
+
+        public async Task CommitAsync(CancellationToken cancellationToken) => await _context.SaveChangesAsync(cancellationToken);
+
+        public async Task RollbackAsync(CancellationToken cancellationToken) => await Task.CompletedTask;
+    }
+}
