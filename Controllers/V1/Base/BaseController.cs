98189ddbcef41528d8f45bc3524a273ceb4ef163
eduardokm <eduardokm@opcaovirtual.com.br>
2023-09-03 00:48:17 -0400

    Feat - Created base controller

---
+++ Controllers/V1/Base/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiroAppAws.Controllers.V1.Base
+{
+    [ApiController]
+    [Route("public/api/[Controller]")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
